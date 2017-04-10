25263b5bbf1c5b7b9ddaef035843f76736c5c9cb
Jake Romer <jkrmr.io@gmail.com>
2017-04-10 09:52:50 -0700

    Add facebook login button

--- buynothing/AppDelegate.swift
+++ buynothing/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    return true
+  }
+
+  func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    _ = FBSDKApplicationDelegate
+      .sharedInstance()
+      .application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String,
+                   annotation: options[UIApplicationOpenURLOptionsKey.annotation] as! String)
     return true
   }
 
--- buynothing/Controllers/AuthController.swift
+++ buynothing/Controllers/AuthController.swift
@@ -7,32 +7,16 @@
 //
 
 import UIKit
-import SafariServices
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class AuthController: UIViewController, SFSafariViewControllerDelegate {
-  @IBOutlet weak var loginButton: UIButton!
-  @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
-
-  var safari: SFSafariViewController!
-  
+class AuthController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-
-//    if let authRequest = FacebookAPI.shared.buildAuthenticationRequest(),
-//      let url = authRequest.url {
-//      safari = SFSafariViewController(url: url)
-//      safari.delegate = self
-//    }
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    guard let safari = safari else { return }
-    present(safari, animated: true, completion: nil)
-  }
-
-  func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-    loadingIndicator.stopAnimating()
+    
+    let facebookLoginButton = FBSDKLoginButton()
+    facebookLoginButton.center = self.view.center
+    view.addSubview(facebookLoginButton)
   }
 }
 
