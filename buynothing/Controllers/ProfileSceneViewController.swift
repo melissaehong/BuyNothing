25bf2f281b1d91f9a1bc5e2a53ba75d13cf516a0
Alex Cahn <alexcahn@gmail.com>
2017-04-11 19:33:24 -0700

    Conflicts solved (#50)
    * profile basic layout
    
    * changing to master
    
    * got xib to load to profile page
    
    * got rid of unnecessary print statement
    
    * profile on 2nd storyboard

--- buynothing/Controllers/ProfileSceneViewController.swift
+++ buynothing/Controllers/ProfileSceneViewController.swift
@@ -9,13 +9,7 @@
 import UIKit
 
 class ProfileSceneViewController: UIViewController {
-    
-    override func loadView() {
-        super.loadView()
-        let profileVC = Bundle.main.loadNibNamed(ProfileSceneViewController.reuseID, owner: self, options: nil)?.first as! ProfileSceneViewController
-        present(profileVC, animated: true, completion: nil)
-        
-    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
