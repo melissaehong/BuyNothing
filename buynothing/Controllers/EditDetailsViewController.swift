987f1bed91753230335d5ae305514a446327d5fa
Annie Ton <annieqton@gmail.com>
2017-04-11 18:05:44 -0700

    Location (#45)
    * adding map container to storyboard
    
    * trying to get the map view in the container
    
    * changed slider

--- buynothing/Controllers/EditDetailsViewController.swift
+++ buynothing/Controllers/EditDetailsViewController.swift
@@ -12,15 +12,45 @@ class EditDetailsViewController: UIViewController {
     @IBOutlet weak var listingTitle: UITextField!
     @IBOutlet weak var listingDescription: UITextField!
 
+
+    @IBOutlet weak var mapContainerView: UIView!
+    
+    @IBOutlet weak var conditionSlider: UISlider!
+    @IBOutlet weak var usedCondition: UILabel!
+    @IBOutlet weak var normalCondition: UILabel!
+    @IBOutlet weak var excellentCondition: UILabel!
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+
+        var currentValue = Int(sender.value)
+        
+        usedCondition.text = "\(currentValue)"
+        normalCondition.text = "\(currentValue)"
+        excellentCondition.text = "\(currentValue)"
+    }
+    
+    
+    
     var listing: Listing!
+    
+    let mapNibView = Bundle.main.loadNibNamed("MapNibCell", owner: self, options: nil)?.first as? UIView
 
     override func viewDidLoad() {
         super.viewDidLoad()
         listingTitle.text = listing.title
         listingDescription.text = listing.descriptionText
+        
+        self.mapContainerView.addSubview(mapNibView!)
+      
+
+        
+        //        self.mapContainerView.register(mapNibCell, forCellReuseIdentifier: MapNibCell.identifier)
+//        
     }
 
+    
     @IBAction func saveButtonPressed(_ sender: Any) {
+    
         listing.title = listingTitle.text ?? ""
         listing.descriptionText = listingDescription.text ?? ""
 
